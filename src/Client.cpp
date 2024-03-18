eeee2de6a10969b74dff6aae84bf3dd459602d01
Ivan <i.lin@mail.utoronto.ca>
2024-03-18 08:47:14 +0800

    Merge ros-multiwii with msp

--- src/Client.cpp
+++ src/Client.cpp
@@ -11,7 +11,7 @@ Client::Client() :
     port(io),
     log_level_(SILENT),
     msp_ver_(1),
-    fw_variant(FirmwareVariant::INAV) {}
+    fw_variant(FirmwareVariant::BAFL) {}
 
 Client::~Client() {}
 
